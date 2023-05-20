0180fca0d959092a80deb947c14b9826d7952486
June <sungj1207@gmail.com>
2023-05-20 17:48:40 +0900

    순차적 애니메이션 - 순차적으로 요소 보이기

--- js/main.js
+++ js/main.js
@@ -46,3 +46,13 @@ window.addEventListener('scroll', _.throttle(function () {
     });
   }
 }, 300));
+
+
+// [index.html] VISUAL 애니메이션 효과 fade-in
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.7 -> 순차적으로 애니메이션 효과
+    opacity: 1
+  });
+});
