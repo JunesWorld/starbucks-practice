81cede246395adffe3cbe8d9a5842e66fb2a08fc
June <sungj1207@gmail.com>
2023-05-23 15:58:43 +0900

    유튜브 영상 배경 - 반복 애니메이션

--- js/main.js
+++ js/main.js
@@ -96,3 +96,26 @@ promotionToggleBtn.addEventListener('click', function() {
     promotionEl.classList.remove('hide');
   }
 });
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `.parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 반환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2)) 
+}
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작시간
+    { // 옵션
+      y: size,
+      repeat: -1, // 무한 반복
+      yoyo: true, // 한번 재상한 애니메이션을 다시 뒤로 재생해서 떠 있는 느낌
+      ease: Power1.easeInOut, // 움직임 제어
+      delay: random(0, delay)
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
